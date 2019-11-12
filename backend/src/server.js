41caad7a283928e144c580520e88ba16c648ea92
JorbFreire <jorbfreire@ufrn.edu.br>
2019-11-12 00:05:50 -0300

    security improvement

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const mongoConect = require('./config/mongodbConfig');
+
 const routes = require('./routes');
 const app = express();
 const door = 8080;
 
-mongoose.connect('mongodb+srv://routine_reader:pgNxPmmEH7dxrEY@routine-4di1x.mongodb.net/test?retryWrites=true&w=majority',{
-    // senh: pgNxPmmEH7dxrEY
+
+mongoose.connect(mongoConect.url ,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
